aa8599a208f4bab74ab1b6661cb4a8aafa1cdbaf
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-06-21 10:14:52 +0200

    added prettier & stylelint (+ exact package versions)

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+export default {
+  printWidth: 512,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  vueIndentScriptAndStyle: false,
+}
